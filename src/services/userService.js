38484d3a21bfdcceda9a9f67447724f45ea1114c
zxckhanh <kvolleyballzz23@gmail.com>
2026-03-23 02:13:59 +0700

    feat: khởi tạo cấu trúc thư mục và các file service cho 6 chức năng Admin và User

---
+++ src/services/userService.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'http://127.0.0.1:8080/api';
+
+export const userService = {
+  uploadCCCD: async (formData) => { /* Chờ code ngày mai */ },
+  getBalance: async (userId) => { /* Chờ code ngày mai */ },
+  adminGetAllUsers: async () => { /* Chờ code ngày mai */ },
+  adminApproveKYC: async (kycId, status) => { /* Chờ code ngày mai */ },
+  // ... các hàm khác cho đủ 6 chức năng
+};
